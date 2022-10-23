80351103e7c5644730b7b909c290170fa5d99580
natesway <64743334+natesway@users.noreply.github.com>
2022-10-23 15:11:07 -0400

    Update fuchsia-build.sh

--- fuchsia-build.sh
+++ fuchsia-build.sh
@@ -1,10 +1,10 @@
 curl -sO https://storage.googleapis.com/fuchsia-ffx/ffx-linux-x64 && chmod +x ffx-linux-x64 && ./ffx-linux-x64 platform preflight
 sudo apt install curl file git unzip
 curl -s "https://fuchsia.googlesource.com/fuchsia/+/HEAD/scripts/bootstrap?format=TEXT" | base64 --decode | bash
-export PATH=~/fuchsia/.jiri_root/bin:$PATH
-source ~/fuchsia/scripts/fx-env.sh
+export PATH=/home/runner/work/fuchsiaOS/fuchsiaOS/fuchsia/.jiri_root/bin:$PATH
+source /home/runner/work/fuchsiaOS/fuchsiaOS/fuchsia/scripts/fx-env.sh
 source ~/.bash_profile
-sudo ln -s ~/fuchsia/scripts/fx /usr/bin
+sudo ln -s /home/runner/work/fuchsiaOS/fuchsiaOS/fuchsia/scripts/fx /usr/bin
 fx setup-ufw
 fx set workstation_eng.chromebook-x64 --release
 fx goma
